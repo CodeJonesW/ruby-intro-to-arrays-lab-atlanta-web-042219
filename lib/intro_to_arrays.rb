89dc7f6a815c08692c27360bf38c5feb1be121ca
Will Jones <willjones2010@gmail.com>
2019-03-18 18:50:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,8 +8,16 @@ def array_with_two_elements
   return newArray
 end
 
-first_element (array)
-  array[0]
+def first_element (array)
+  return array[0]
 end
+
+def third_element(array)
+  return array[2]
 end
+
+def last_element(array)
+  return array[-1]
+end
+
 
